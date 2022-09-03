2ef98b0d451091bdd87840469db44a45708761db
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-02 21:02:15 -0300

    feat: adição da configuração de conexão com o db hospedado

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
+const options = {
+  dbName: 'tractian',
+};
+
 const connectToDatabase = (mongoDatabaseURI = process.env.MONGO_URI
   || 'mongodb://localhost:27017/tractian') => mongoose
-  .connect(mongoDatabaseURI);
+  .connect(mongoDatabaseURI, options);
 
 export default connectToDatabase;
