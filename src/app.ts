cb991b48f4814ec99aa84b19c3176db50809b4e6
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-04 15:57:03 -0300

    feat: inclusão da rota para gerenciar usuários

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,13 @@ import swaggerFile from '../swagger_output.json';
 import 'express-async-errors';
 import errorHandler from './middleware/erros/errorHandler';
 import asetRoute from './routes/aset';
+import userRoute from './routes/user';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(asetRoute);
+app.use(userRoute);
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(errorHandler);
 
