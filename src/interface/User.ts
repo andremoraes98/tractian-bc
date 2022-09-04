1c68a299acf85b6f6a82bb0011e3100989db0582
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-04 10:49:01 -0300

    feat: implementação do zod para inferir a interface User

--- src/interface/User.ts
+++ src/interface/User.ts
@@ -1,6 +1,10 @@
-interface IUser {
-  user: string,
-  unit: string,
-}
+import { z } from 'zod';
+
+const userSchema = z.object({
+  user: z.string(),
+  unit: z.string(),
+});
+
+type IUser = z.infer<typeof userSchema>;
 
 export default IUser;
